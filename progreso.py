c379f5c21c8aa0adbd0417cb2f35bcfd81309b28
edril023 <130479063+edril023@users.noreply.github.com>
2024-04-29 15:22:33 -0300

    Add files via upload

---
+++ progreso.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.progressbar import ProgressBar
+
+class MyApp (App):
+    def build(self):
+        return ProgressBar(value=89)
+    
+if __name__ == "__main__":
+    MyApp().run()
